2d539471514f9d8aded775d55ead5a1abd8d1683
Zerovaskr <95402253+Zerovaskr@users.noreply.github.com>
2022-01-10 15:16:57 -0600

    Update handle.bat

--- handle.bat
+++ handle.bat
@@ -5,7 +5,7 @@ set count2=0
 start sound.vbs
 
 :change1
-if %count% gtr 24 goto a
+if %count% gtr 30 goto a
 start change.vbs
 set /a count=%count%+1
 goto change1
@@ -26,7 +26,7 @@ timeout /nobreak 7
 goto a
 
 :change2
-if %count2% gtr 24 goto done
+if %count2% gtr 30 goto done
 start change2.vbs
 set /a count2=%count2%+1
 goto change2
